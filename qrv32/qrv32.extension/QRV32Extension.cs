affc21975a3b5b93360de70a2863a0b856025391
Evgeny Muryshkin <evmuryshkin@gmail.com>
2022-02-12 12:49:42 +1100

    fix compilation, qrv32 toolbar extension

---
+++ qrv32/qrv32.extension/QRV32Extension.cs
@@ -0,0 +1,16 @@
+﻿using QRV32.CPU;
+using Quokka.Core.Bootstrap;
+using Quokka.Extension.Interop;
+
+namespace qrv32.extension
+{
+    [ExtensionClass]
+    public class QRV32Extension
+    {
+        [ExtensionMethod(icon: TopLevelIcon.Translate)]
+        public static void Translate()
+        {
+            QuokkaRunner.FromConfig(Env.RTLVerilogConfig, new[] { nameof(RISCVModule) });
+        }
+    }
+}
